7017ae14047214755fd559cb563e3230f11f7f11
Leonardo de Freitas <leonardo.freitas777x@gmail.com>
2024-10-11 17:21:15 -0500

    Password test

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ while gameloop:
         if sprite_interval >= 5:
             sprite_interval = 5
 
-    screen.blit(current_sprite, (50, 169))  # Draw the current sprite
+    screen.blit(current_sprite, (50, 169))
 
     clock.tick(30)
     pygame.display.update()
